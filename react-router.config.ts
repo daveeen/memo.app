f99e681799524a0ff7dd6778986a6d6542ab6d42
YerielShantuy <yerielph@gmail.com>
2026-07-21 01:49:59 +1000

    chore: SPA mode + core deps

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,14 +1,2 @@
 import type { Config } from "@react-router/dev/config";
-
-export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
-  future: {
-    v8_middleware: true,
-    v8_passThroughRequests: true,
-    v8_splitRouteModules: true,
-    v8_trailingSlashAwareDataRequests: true,
-    v8_viteEnvironmentApi: true,
-  },
-} satisfies Config;
+export default { ssr: false } satisfies Config; // SPA — all logic client-side
